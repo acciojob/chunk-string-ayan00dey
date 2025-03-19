c3db7351426679df8cc5dd1f49a7dbe86d1ddf95
ayan00dey <mr.dey.ayanava@gmail.com>
2025-03-19 23:27:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function stringChop(str, size) {
   // your code here
+	if(!str) return[];
+	if(size<0) return [];
+	let arr = [];
+	for(let i=0;i<str.length;i+=size){
+		arr.push(str.substring(i,i+size));
+	}
+	return arr;
+	
 }
 
 // Do not change the code below
